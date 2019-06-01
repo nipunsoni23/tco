1637f3495b7d4f6da84a89a37cbe78363d087d35
ektasaini <34946048+ektasaini@users.noreply.github.com>
2019-06-01 12:42:31 +0530

    admin,learner,trainer

---
+++ models/admin.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Shape of User document.
+const adminSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name:{
+      type: String,
+      required:true
+  }
+  
+});
+
+// Create model of the user schema
+module.exports = mongoose.model("learner", learnerSchema);
---
+++ models/learner.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Shape of User document.
+const learnerSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name:{
+      type: String,
+      required:true
+  },
+  subjectTrainer: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "trainer"
+    }
+  ]
+});
+
+// Create model of the user schema
+module.exports = mongoose.model("learner", learnerSchema);
